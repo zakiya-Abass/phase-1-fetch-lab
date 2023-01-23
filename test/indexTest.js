3def44d7d6f81487cbb7dad41fb9adb671df378b
Zakiya-Abass <zakiyaabass@icloud.com>
2023-01-23 23:43:17 +0300

    fetch-lab

--- test/indexTest.js
+++ test/indexTest.js
@@ -1,33 +1,33 @@
-require ( './helpers.js' );
+require("./helpers.js");
 
-const sinon = require( 'sinon' );
-const helpers = require( './helpers' );
-const chai = require( 'chai' );
-const spies = require( 'chai-spies' );
+const sinon = require("sinon");
+const helpers = require("./helpers");
+const chai = require("chai");
+const spies = require("chai-spies");
 
-chai.use( spies );
+chai.use(spies);
 
+describe("index.js", () => {
+  describe("fetchBooks()", () => {
+    beforeEach(() => {
+      window.document.body.innerHTML = "<main></main>";
+      window.fetch = require("node-fetch");
+    });
 
-describe( "index.js", () => {
-  describe( 'fetchBooks()', () => {
+    it("sends a fetch request to 'https://anapioficeandfire.com/api/books'", async () => {
+      chai.spy.on(window, "fetch");
+      await fetchBooks();
+      expect(
+        window.fetch,
+        "A fetch to the API was not found"
+      ).to.have.been.called.with("https://anapioficeandfire.com/api/books");
+    });
 
-    beforeEach( () => {
-      window.document.body.innerHTML = '<main></main>'
-      window.fetch = require( 'node-fetch' );
-    } );
-
-    it( "sends a fetch request to 'https://anapioficeandfire.com/api/books'", async () => {
-      chai.spy.on( window, 'fetch' );
-      await fetchBooks()
-      expect( window.fetch, "A fetch to the API was not found" )
-        .to.have.been.called.with( 'https://anapioficeandfire.com/api/books' );
-    } )
-
-    it( "renders book titles into the DOM by passing a JSON object to renderBooks()", async () => {
-      chai.spy.on( window, 'renderBooks' );
+    it("renders book titles into the DOM by passing a JSON object to renderBooks()", async () => {
+      chai.spy.on(window, "renderBooks");
       await fetchBooks().then(() => {
-        expect( window.renderBooks ).to.have.been.called();
-      })
-    } )
-  } )
-})
+        expect(window.renderBooks).to.have.been.called();
+      });
+    });
+  });
+});
